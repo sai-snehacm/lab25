baa053707a29f829e7ddbc3d4169aba24f99b27e
sai-snehacm <saisneha.cm2502@gmail.com>
2025-06-28 06:14:04 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    printf("Enter two integers: ");
+    if (scanf("%d %d", &a, &b) == 2) {
+        int sum = a + b;
+        printf("%d + %d = %d\n", a, b, sum);
+    }
+    return 0;
+}
